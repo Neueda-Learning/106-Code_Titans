0e95d3f451c98ed1eee05c4a6bc9c3cd4d0d0144
Gourav-Parida <gouravkumarparida@gmail.com>
2026-07-31 09:38:49 +0000

    first commit

---
+++ backend/src/main/java/com/neueda/__Code_Titans/Application.java
@@ -0,0 +1,13 @@
+package com.neueda.__Code_Titans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
